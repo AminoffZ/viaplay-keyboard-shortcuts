bd23e3b50ad51f68dd92f7fe71aebf4810be5621
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2022-09-10 12:18:10 +0300

    Remove project files

--- src/scripts/content.ts
+++
@@ -1,7 +0,0 @@
-/* We inject script.js */
-const script: HTMLScriptElement = document.createElement("script");
-script.src = chrome.runtime.getURL("script.js");
-(document.head || document.documentElement).appendChild(script);
-script.onload = function () {
-  script.remove();
-};
